fc658267ae2ce9252836bc5812e3011639890607
Indu Sree <Indu Sree@DESKTOP-UN8GH21>
2024-08-12 14:49:14 +0530

    Add project files.

---
+++ NullReferenceException/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NullReferenceEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            try
+            {
+                string name = "Indu";
+                //string name = null; // Name should not be given null as the value
+
+                Console.WriteLine(name.Length);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("Input string is Null");
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("----------------------");
+
+            Console.WriteLine("This line will run only when the error is handled.");
+
+        }
+    }
+}
